ccf725160f2f4ccb6d53cc270331edf822163d62
noahd <noah.decroix3@gmail.com>
2022-10-25 10:27:44 +0200

    Add controller

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -1,1 +1,15 @@
 <?php
+
+namespace App\Controller;
+
+class ErrorController extends AbstractController {
+
+    public function index()
+    {
+        // TODO: Implement index() method.
+    }
+
+    public function error404() {
+
+    }
+}
--- view/base.html.php
+++ view/base.html.php
@@ -1,1 +1,36 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Forum</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+
+<body>
+    <header>
+        <div>
+            <h1>Forum</h1>
+        </div>
+
+        <div>
+            <div>
+                <a href=""></a>
+            </div>
+
+            <div>
+                <a href=""></a>
+            </div>
+
+        </div>
+
+    </header>
+    <main><?= $data ?></main>
+
+
+    <script src="/assets/js/app.js"></script>
+</body>
+
+</html>
