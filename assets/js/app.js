310d8b1385e4472b0e52dddb559e177656e6784f
noahd <noah.decroix3@gmail.com>
2022-11-14 14:05:48 +0100

    contact page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
 import '../../assets/css/style.css';
 import anime from 'animejs/lib/anime.es.js';
 
+const contactButton = document.querySelector("#contactButton");
+
 anime({
     targets: '#homeTittle',
     fontSize: ["0rem", "3rem"],
@@ -9,7 +11,7 @@ anime({
 });
 
 anime({
-    targets: '#test',
+    targets: '#test, .contact_form',
     translateX: ["0rem", "5rem"],
     delay: 100,
     duration: 1000,
--- view/user/contact.html.php
+++ view/user/contact.html.php
@@ -1,7 +1,25 @@
 <div class="container">
     <div>
         <h1 id="test">Contact</h1>
-        <form action="" method="post">
+        <form class="contact_form" action="/?c=user&a=contact" method="post">
+            <div>
+                <label for="email">Adresse mail du compte: </label>
+                <input type="email" name="email" id="email" placeholder="Adresse mail ...">
+            </div>
+
+            <div>
+                <label for="object">Objet: </label>
+                <input type="text" name="object" id="object" placeholder="Objet ...">
+            </div>
+
+            <div>
+                <label for="content">Contenu: </label>
+                <textarea name="content" id="content" cols="30" rows="10"></textarea>
+            </div>
+
+            <div>
+                <button id="contactButton">Envoyer</button>
+            </div>
 
         </form>
     </div>
